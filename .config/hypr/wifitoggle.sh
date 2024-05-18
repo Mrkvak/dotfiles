a4c990df929c3bf8de0679f3c6bceb5c17a170d5
Radek Pilar <github@mrkva.eu>
2024-05-18 21:11:05 +0200

    Fix rfkill race, added mc selenzied theme

--- .config/hypr/wifitoggle.sh
+++ .config/hypr/wifitoggle.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if sudo /etc/init.d/wpa_supplicant status >/dev/null; then
 	sudo /etc/init.d/wpa_supplicant stop
-	sudo /usr/local/sbin/rfkill phy0 0
+	sleep 3
+	sudo /usr/local/sbin/rfkill.sh phy0 0
 else
 	sudo /etc/init.d/wpa_supplicant start
-	sudo /usr/local/sbin/rfkill phy0 1
+	sleep 3
+	sudo /usr/local/sbin/rfkill.sh phy0 1
 fi
 
